634705ce5c2588fdb6033cd63ae00afad5f1f26f
Nicolas <nicolasrodriguezch@hotmail.com>
2020-11-14 11:29:52 -0500

    updates app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set("views", "views");
 
 app.get("/", (req, res)=>{ 
     const texts =[]; 
-        for (let i = 0; i <= 50; i++) {
+        for (let i = 1; i <= 50; i++) {
             texts.push(`${i} Soy ${(i % 2 === 0 ? "Par!" : "Impar!")}`)
         }
     res.render("index", { texts: texts })
